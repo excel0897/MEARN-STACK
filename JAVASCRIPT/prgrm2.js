2ca3841c79866fa2cfeea08394032eb61a97c4d5
excel0897 <aiswaryalakshmyav@gmail.com>
2024-05-29 14:46:34 +0530

    initial commit

---
+++ JAVASCRIPT/prgrm2.js
@@ -0,0 +1,16 @@
+var name="Aiswarya";
+console.log(name)
+console.log(typeof name)
+var num=("123");
+console.log(num)
+console.log(typeof num)
+var torf=true
+console.log(typeof torf)
+var profession;
+console.log(typeof profession)
+console.log(typeof x)
+var obj = {
+    name:"Aiswarya",
+    age:"24",
+}
+console.log(typeof obj);
